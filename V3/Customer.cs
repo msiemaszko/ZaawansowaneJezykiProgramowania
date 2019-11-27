1eed8c0f3b7dc7212e33208147e4628cc391427a
Marek <marecki.mail@gmail.com>
2019-11-27 18:00:28 +0100

    v3 init

---
+++ V3/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace V3
+{
+    public class Customer
+    {
+
+        private readonly string name;
+        private List<Rental> rentals = new List<Rental>();
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddRental(Rental rental)
+        {
+            rentals.Add(rental);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string Statement()
+        {
+            double totalAmount = 0;
+            int frequentRenterPoints = 0;
+            string result = "Rental Record for " + GetName() + "\n";
+
+            foreach (Rental rental in rentals)
+            {
+                // cena za pozycje wyporzyczenia
+                double thisAmount = rental.CountPrice();
+
+                // dolicz punkty stalego klienta
+                frequentRenterPoints += rental.CountPoints();
+
+                // show figures for this rental
+                result += "\t" + rental.GetMovie().GetTitle() + "\t" + thisAmount.ToString() + "\n";
+                totalAmount += thisAmount;
+            }
+
+            // add footer lines
+            result += "Amount owed is " + totalAmount.ToString() + "\n";
+            result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points";
+
+            return result;
+        }
+    }
+}
---
+++ V3/Rental.cs
@@ -0,0 +1,35 @@
+﻿namespace V3
+{
+    public class Rental
+    {
+        private Movie movie;
+        private readonly int daysRented;
+
+        public Rental(Movie movie, int daysRented)
+        {
+            this.movie = movie;
+            this.daysRented = daysRented;
+        }
+
+        public int GetDaysRented()
+        {
+            return daysRented;
+        }
+
+        public Movie GetMovie()
+        {
+            return movie;
+        }
+
+        public int CountPoints()
+        {
+            return movie.CountPoints(daysRented);
+
+        }
+
+        public double CountPrice()
+        {
+            return movie.CountPrice(daysRented);
+        }
+    }
+}
